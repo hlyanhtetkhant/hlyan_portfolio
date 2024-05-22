c211e9a2e3cb7fba86f28074bd38ffde16b05c15
hlyanhtetkhant <116857193+hlyanhtetkhant@users.noreply.github.com>
2024-05-22 13:34:33 +0630

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function seeMore() {
+    window.location.href = 'html2.html';
+}
+function seeMore2() {
+    window.location.href = 'html3.html';
+}
+function seeMore3() {
+    window.location.href = 'html4.html';
+}
+function seeMore4() {
+    window.location.href = 'html5.html';
+}
+function seeMore5() {
+    window.location.href = 'html1.html';
+}
+
